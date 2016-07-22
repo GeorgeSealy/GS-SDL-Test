b2d69f0c672bb8ec849592edfd2af9c3023be16a
George Sealy <george@mixbit.com>
2016-07-22 15:53:20 +1200

    Initial Commit

---
+++ GL_SDL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GL_SDL
+//
+//  Created by George Sealy on 22/07/16.
+//  Copyright © 2016 MixBit. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
